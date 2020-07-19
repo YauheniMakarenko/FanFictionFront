b8e144d7382fa2b278f2e58207d0bfad138d32c5
Yauheni Makarenka <loxix_1996@mail.ru>
2020-07-19 22:05:34 +0300

    Commit

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 const API_URL = 'https://fanfictionfback.herokuapp.com/api/test/';
 
