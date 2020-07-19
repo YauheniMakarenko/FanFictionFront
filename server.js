2c8af3e3b599ec3462a90b394e2f8b4815f42b00
Yauheni Makarenka <loxix_1996@mail.ru>
2020-07-19 23:42:18 +0300

    Commit 10

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 //Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
