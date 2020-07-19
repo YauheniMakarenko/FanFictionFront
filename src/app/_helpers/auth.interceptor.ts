24b65724a2ed0e908f924765bc64fccf8d167fa1
Yauheni Makarenka <loxix_1996@mail.ru>
2020-07-19 21:15:28 +0300

    Second Commit

--- src/app/_helpers/auth.interceptor.ts
+++ src/app/_helpers/auth.interceptor.ts
@@ -1,8 +1,7 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
-import { TokenStorageService } from '../_services/token-storage.service';
+import {TokenStorageService} from '../_services/token-storage.service';
 
 const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
 // const TOKEN_HEADER_KEY = 'x-access-token';   // for Node.js Express back-end
