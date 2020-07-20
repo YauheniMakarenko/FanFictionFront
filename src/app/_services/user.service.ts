d10a77cbcf0da7e2f71c38790db707547e52b71e
Yauheni Makarenka <loxix_1996@mail.ru>
2020-07-20 23:01:44 +0300

    Commit 18

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = 'https://fanfictionfback.herokuapp.com/api/test/';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
